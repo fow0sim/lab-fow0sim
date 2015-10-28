39919dc1276866bdfccc06530085b17c846267b6
Thomas Simmig <thomas.simmig@volkswagen.de>
2015-10-28 08:43:33 +0100

    Added Spring Initializer

---
+++ src/main/java/eu/simmig/lab/config/SpringApplicationContextInitializer.java
@@ -0,0 +1,27 @@
+package eu.simmig.lab.config;
+
+import org.springframework.cloud.Cloud;
+import org.springframework.cloud.CloudException;
+import org.springframework.cloud.CloudFactory;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+
+public class SpringApplicationContextInitializer implements ApplicationContextInitializer<AnnotationConfigWebApplicationContext> {
+
+    @Override
+    public void initialize(AnnotationConfigWebApplicationContext applicationContext) {
+        Cloud cloud = getCloud();
+        ConfigurableEnvironment appEnvironment = applicationContext.getEnvironment();
+    }
+
+    private Cloud getCloud() {
+        try {
+            CloudFactory cloudFactory = new CloudFactory();
+            return cloudFactory.getCloud();
+        } catch (CloudException ce) {
+            return null;
+        }
+    }
+
+}
